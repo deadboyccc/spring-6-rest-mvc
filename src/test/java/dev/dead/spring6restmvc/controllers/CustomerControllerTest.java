7790faee405e6d74b0895905ca509abc920d5060
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-25 21:35:50 +0300

    enhance CustomerControllerTest with test for createNewCustomer endpoint and Jackson serialization validation

--- src/test/java/dev/dead/spring6restmvc/controllers/CustomerControllerTest.java
+++ src/test/java/dev/dead/spring6restmvc/controllers/CustomerControllerTest.java
@@ -1,5 +1,6 @@
 package dev.dead.spring6restmvc.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.dead.spring6restmvc.models.Customer;
 import dev.dead.spring6restmvc.services.CustomerService;
 import dev.dead.spring6restmvc.services.CustomerServiceImpl;
@@ -12,9 +13,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -22,18 +27,43 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class CustomerControllerTest {
 
     private final String basePath = "/api/v1/customer";
-    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+    CustomerServiceImpl customerServiceImpl;
     @MockitoBean
     CustomerService customerService;
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
+        customerServiceImpl = new CustomerServiceImpl();
     }
 
     @AfterEach
     void tearDown() {
+        customerServiceImpl.getCustomers().clear();
+    }
+
+    @Test
+    void createNewCustomer() throws Exception {
+        Customer customerPostDto = Customer.builder()
+                .customerName("TEST")
+                .build();
+        Customer returnedCustomerEntity = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName(customerPostDto.getCustomerName())
+                .build();
+        given(customerService.saveNewCustomer(any(Customer.class)))
+                .willReturn(returnedCustomerEntity);
+        mockMvc.perform(post(basePath)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customerPostDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location",
+                        basePath + "/" + (returnedCustomerEntity.getId().toString())));
     }
 
     @Test
