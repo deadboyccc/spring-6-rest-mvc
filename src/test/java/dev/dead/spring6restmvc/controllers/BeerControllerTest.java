c2b956f9b872c117fe7c1cbb58e085dcc2815f50
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-25 21:17:05 +0300

    enhance BeerControllerTest with test for createNewBeer endpoint and Jackson serialization validation

--- src/test/java/dev/dead/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/dev/dead/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,6 +1,9 @@
 package dev.dead.spring6restmvc.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.dead.spring6restmvc.models.Beer;
+import dev.dead.spring6restmvc.models.BeerStyle;
 import dev.dead.spring6restmvc.services.BeerService;
 import dev.dead.spring6restmvc.services.BeerServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +14,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -28,6 +38,47 @@ class BeerControllerTest {
     final String basePath = "/api/v1/beer";
 
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testCreateNewBeer() throws Exception {
+        Beer beer = Beer.builder()
+                .beerName("NEW BEER TEST")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("123456")
+                .price(new BigDecimal("10.99"))
+                .quantityOnHand(24)
+                .build();
+
+        Beer savedBeer = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName(beer.getBeerName())
+                .beerStyle(beer.getBeerStyle())
+                .upc(beer.getUpc())
+                .price(beer.getPrice())
+                .quantityOnHand(beer.getQuantityOnHand())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        given(beerService.saveNewBeer(any(Beer.class))).willReturn(savedBeer);
+
+        mockMvc.perform(post(basePath)
+                        .content(objectMapper.writeValueAsString(beer))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", "/api/v1/beer/" + savedBeer.getId().toString()));
+    }
+
+    void testJacksonConfig() throws JsonProcessingException {
+        Beer beer = beerServiceImpl.getBeers().get(0);
+        String jsonString = objectMapper.writeValueAsString(beer);
+        assertEquals(beer, objectMapper.readValue(jsonString, Beer.class));
+    }
     @Test
     void getBeerByIdTest() throws Exception {
         Beer beer = beerServiceImpl.getBeers().get(0);
