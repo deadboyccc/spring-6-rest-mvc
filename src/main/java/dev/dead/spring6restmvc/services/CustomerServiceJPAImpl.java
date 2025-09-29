34fb8884379ca58ef1cd78bae9dff77401848794
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-29 19:56:03 +0300

    add `CustomerServiceJPAImpl` implementation for `CustomerService` with basic CRUD operations

---
+++ src/main/java/dev/dead/spring6restmvc/services/CustomerServiceJPAImpl.java
@@ -0,0 +1,58 @@
+package dev.dead.spring6restmvc.services;
+
+import dev.dead.spring6restmvc.mappers.CustomerMapper;
+import dev.dead.spring6restmvc.models.CustomerDTO;
+import dev.dead.spring6restmvc.repositories.BeerRepository;
+import dev.dead.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPAImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+    private final BeerRepository beerRepository;
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.ofNullable(customerMapper.customerToCustomerDTO(customerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public List<CustomerDTO> getCustomers() {
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO updateCustomerById(UUID customerId, CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomer(UUID customerId, CustomerDTO customerDTO) {
+
+    }
+}
