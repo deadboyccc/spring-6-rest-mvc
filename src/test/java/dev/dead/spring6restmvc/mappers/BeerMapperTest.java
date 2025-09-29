39b717b6ebc655fbb8838e38b8393599fda306cf
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-29 07:06:19 +0300

    update `@SpringBootTest` configuration in `CustomerMapperTest` and `BeerMapperTest` to use their respective implementations

--- src/test/java/dev/dead/spring6restmvc/mappers/BeerMapperTest.java
+++ src/test/java/dev/dead/spring6restmvc/mappers/BeerMapperTest.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = CustomerMapper.class)
+@SpringBootTest(classes = BeerMapperImpl.class)
 class BeerMapperTest {
     @Autowired
     BeerMapper beerMapper;
--- src/test/java/dev/dead/spring6restmvc/mappers/CustomerMapperTest.java
+++ src/test/java/dev/dead/spring6restmvc/mappers/CustomerMapperTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = CustomerMapper.class)
+@SpringBootTest(classes = CustomerMapperImpl.class)
 class CustomerMapperTest {
     @Autowired
     CustomerMapper customerMapper;
