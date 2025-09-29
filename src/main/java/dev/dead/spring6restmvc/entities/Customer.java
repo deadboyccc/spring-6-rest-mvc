2542b37818e78ceb1829259e67b7e53cf119b4ce
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-29 05:57:26 +0300

    add JPA repositories for Beer and Customer to support database interactions via Spring Data JPA

--- src/main/java/dev/dead/spring6restmvc/entities/Customer.java
+++ src/main/java/dev/dead/spring6restmvc/entities/Customer.java
@@ -1,12 +1,21 @@
 package dev.dead.spring6restmvc.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
