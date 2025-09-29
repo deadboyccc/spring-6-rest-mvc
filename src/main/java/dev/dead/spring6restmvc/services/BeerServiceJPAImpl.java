a90724b3a83abbb8f074832128aaed2e7de1fa33
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-29 19:53:26 +0300

    add `BeerServiceJPAImpl` implementation for `BeerService` with basic CRUD operations

---
+++ src/main/java/dev/dead/spring6restmvc/services/BeerServiceJPAImpl.java
@@ -0,0 +1,57 @@
+package dev.dead.spring6restmvc.services;
+
+import dev.dead.spring6restmvc.mappers.BeerMapper;
+import dev.dead.spring6restmvc.mappers.BeerMapperImpl;
+import dev.dead.spring6restmvc.models.BeerDTO;
+import dev.dead.spring6restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPAImpl implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.ofNullable(beerMapper.beerToBeerDTO(beerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public List<BeerDTO> getBeers() {
+        return
+                beerRepository.findAll()
+                        .stream()
+                        .map(beerMapper::beerToBeerDTO)
+                        .collect(Collectors.toList());
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beerDTO) {
+        return beerMapper.beerToBeerDTO(beerRepository.save(beerMapper.beerDTOToBeer(beerDTO)));
+    }
+
+    @Override
+    public BeerDTO updateBeer(UUID beerId, BeerDTO beerDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteBeerById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beerDTO) {
+
+    }
+}
