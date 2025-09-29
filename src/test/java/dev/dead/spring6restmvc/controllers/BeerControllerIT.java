a8f6fd4ea8336581ae06ddfb6e13aaf2a88f536a
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-30 00:06:37 +0300

    add integration test to `BeerControllerIT` for saving a new beer including validation of saved data and timestamps

--- src/test/java/dev/dead/spring6restmvc/controllers/BeerControllerIT.java
+++ src/test/java/dev/dead/spring6restmvc/controllers/BeerControllerIT.java
@@ -2,14 +2,20 @@ package dev.dead.spring6restmvc.controllers;
 
 import dev.dead.spring6restmvc.entities.Beer;
 import dev.dead.spring6restmvc.models.BeerDTO;
+import dev.dead.spring6restmvc.models.BeerStyle;
 import dev.dead.spring6restmvc.repositories.BeerRepository;
 import dev.dead.spring6restmvc.services.BeerService;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,6 +28,40 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Rollback
+    @Transactional
+    @Test
+    void saveNewBeer() {
+        BeerDTO beerDTO = returnBeerDto();
+        ResponseEntity responseEntity = beerController.addBeer(beerDTO);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getHeaders()
+                .getLocation());
+
+        var locationPath = responseEntity.getHeaders()
+                .getLocation()
+                .getPath();
+
+        UUID savedUUID = UUID.fromString(locationPath.split("/")[4]);
+        Beer savedBeer = beerRepository.findById(savedUUID)
+                .get();
+        assertNotNull(savedBeer);
+
+
+        assertEquals(beerDTO.getBeerName(), savedBeer.getBeerName());
+        assertEquals(beerDTO.getBeerStyle(), savedBeer.getBeerStyle());
+        assertEquals(beerDTO.getPrice(), savedBeer.getPrice());
+        assertEquals(beerDTO.getQuantityOnHand(), savedBeer.getQuantityOnHand());
+        assertEquals(beerDTO.getUpc(), savedBeer.getUpc());
+        assertEquals(0, savedBeer.getVersion());
+        // refactor
+        assertNotNull(savedBeer.getCreatedAt());
+        assertNotNull(savedBeer.getUpdatedAt());
+
+
+    }
 
     @Test
     void getBeerByIdNotFound() {
@@ -63,5 +103,37 @@ class BeerControllerIT {
         assertNotNull(beerRepository);
         List<BeerDTO> dtos = beerController.getBeers();
         assertEquals(0, dtos.size());
+    }
+
+    BeerDTO returnBeerDto() {
+
+        // uuid+version not set | timestamp set (need change)
+        return BeerDTO.builder()
+                .beerName("New Beer")
+                .beerStyle(BeerStyle.LAGER)
+                .upc(UUID.randomUUID()
+                        .toString())
+                .price(new BigDecimal(12))
+                .quantityOnHand(12)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+    }
+
+    Beer returnBeerEntity() {
+
+
+        // uuid+version not set | timestamp set (need change)
+        return Beer.builder()
+                .beerName("New Beer")
+                .beerStyle(BeerStyle.LAGER)
+                .upc(UUID.randomUUID()
+                        .toString())
+                .price(new BigDecimal(12))
+                .quantityOnHand(12)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
     }
 }
