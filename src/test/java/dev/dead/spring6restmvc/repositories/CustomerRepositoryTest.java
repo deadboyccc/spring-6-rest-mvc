0d68fc08332b83b6da71099198383225f0e321d3
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-29 06:14:29 +0300

    add `CustomerRepositoryTest` with a save test case

---
+++ src/test/java/dev/dead/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,25 @@
+package dev.dead.spring6restmvc.repositories;
+
+import dev.dead.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer customer = customerRepository.save(Customer.builder().customerName("Test Customer").build());
+        assertNotNull(customer);
+        assertNotNull(customer.getId());
+        
+    }
+
+}
