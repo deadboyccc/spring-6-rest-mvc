fec37eedfd2cb3c05563cb50981bec59a72fc021
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-29 20:29:03 +0300

    add integration tests to BeerControllerIT for fetching beer by ID including not found and found scenarios

--- src/test/java/dev/dead/spring6restmvc/controllers/BeerControllerIT.java
+++ src/test/java/dev/dead/spring6restmvc/controllers/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package dev.dead.spring6restmvc.controllers;
 
+import dev.dead.spring6restmvc.entities.Beer;
 import dev.dead.spring6restmvc.models.BeerDTO;
 import dev.dead.spring6restmvc.repositories.BeerRepository;
 import dev.dead.spring6restmvc.services.BeerService;
@@ -10,6 +11,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,13 +19,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class BeerControllerIT {
     @Autowired
     BeerController beerController;
+
     @Autowired
     BeerRepository beerRepository;
 
     @Test
+    void getBeerByIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void getBeerByIdFound() {
+        Beer beer = beerRepository.findAll()
+                .get(0);
+        BeerDTO beerDTO = beerController.getBeerById(beer.getId());
+
+        assertNotNull(beerDTO);
+
+        assertEquals(beer.getId(), beerDTO.getId());
+        assertEquals(beer.getBeerName(), beerDTO.getBeerName());
+        assertEquals(beer.getBeerStyle(), beerDTO.getBeerStyle());
+        assertEquals(beer.getPrice(), beerDTO.getPrice());
+        assertEquals(beer.getQuantityOnHand(), beerDTO.getQuantityOnHand());
+        assertEquals(beer.getUpc(), beerDTO.getUpc());
+    }
+
+    @Test
     void getBeers() {
-        assertNotNull(beerController);
-        assertNotNull(beerRepository);
         List<BeerDTO> dtos = beerController.getBeers();
         assertEquals(3, dtos.size());
     }
