806e70f1b7487750e644bafaff78960b6b4a4d69
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-10-03 23:13:10 +0300

    add exception handler for `MethodArgumentNotValidException` in `ExceptionController` and log validation errors

--- src/main/java/dev/dead/spring6restmvc/controllers/ExceptionController.java
+++ src/main/java/dev/dead/spring6restmvc/controllers/ExceptionController.java
@@ -1,9 +1,16 @@
 package dev.dead.spring6restmvc.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @ControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(NotFoundException.class)
@@ -11,5 +18,23 @@ public class ExceptionController {
         return ResponseEntity
                 .notFound()
                 .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Map<String, String>>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception
+    ) {
+        List<Map<String, String>> errorList = exception.getFieldErrors()
+                .stream()
+                .map(fieldError ->
+                {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                })
+                .toList();
+        log.debug("Method Argument Not Valid Handler - Error list: {}", errorList);
+        return ResponseEntity.badRequest()
+                .body(errorList);
     }
 }
