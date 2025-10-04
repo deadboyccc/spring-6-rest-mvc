b4582c00496e9a81b2f154068781bacbc3bd45bc
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-10-04 03:48:39 +0300

    add validation test for excessively long `beerName` in `BeerRepositoryTest`

--- src/test/java/dev/dead/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/dev/dead/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,10 +2,13 @@ package dev.dead.spring6restmvc.repositories;
 
 import dev.dead.spring6restmvc.entities.Beer;
 import dev.dead.spring6restmvc.models.BeerStyle;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -18,6 +21,31 @@ class BeerRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
 
+    @Rollback
+    @Transactional
+    @Test
+    void testSaveBeerNameTooLongEntityValidation() {
+
+
+        assertThrows(ConstraintViolationException.class, () -> {
+
+            Beer savedBeer = beerRepository.saveAndFlush(
+                    Beer.builder()
+                            .beerName(UUID.randomUUID()
+                                    .toString()
+                                    .repeat(20))
+                            .beerStyle(BeerStyle.LAGER)
+                            .upc(UUID.randomUUID()
+                                    .toString())
+                            .price(new BigDecimal(12))
+                            .quantityOnHand(12)
+                            .build()
+            );
+        });
+    }
+
+    @Rollback
+    @Transactional
     @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepository.saveAndFlush(
