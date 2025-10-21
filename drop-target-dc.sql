99e3349a84c41cd11617430e94ff6ef82850dff3
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-10-21 03:00:35 +0300

    add Flyway migration script and update schema generation; refactor SQL scripts with duplicate entries removal and optimize configuration in application properties

--- drop-target-dc.sql
+++ drop-target-dc.sql
@@ -4,3 +4,33 @@
     drop table if exists beer cascade;
 
     drop table if exists customer cascade;
+
+    set client_min_messages = WARNING;
+
+    drop table if exists beer cascade;
+
+    drop table if exists customer cascade;
+
+    set client_min_messages = WARNING;
+
+    drop table if exists beer cascade;
+
+    drop table if exists customer cascade;
+
+    set client_min_messages = WARNING;
+
+    drop table if exists beer cascade;
+
+    drop table if exists customer cascade;
+
+    set client_min_messages = WARNING;
+
+    drop table if exists beer cascade;
+
+    drop table if exists customer cascade;
+
+    set client_min_messages = WARNING;
+
+    drop table if exists beer cascade;
+
+    drop table if exists customer cascade;
