ae3faa310b7de08a8eeaf21e6ad0d9605ee9e483
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-10-21 05:00:44 +0300

    refactor `PostgresqlTestContainerTests` to use `@ServiceConnection` for simplified Testcontainers integration, deprecating `@DynamicPropertySource` configuration

--- src/test/java/dev/dead/spring6restmvc/repositories/PostgresqlTestContainerTests.java
+++ src/test/java/dev/dead/spring6restmvc/repositories/PostgresqlTestContainerTests.java
@@ -4,6 +4,7 @@ import dev.dead.spring6restmvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -20,19 +21,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 public class PostgresqlTestContainerTests {
     @Container
+    @ServiceConnection
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.10");
     @Autowired
     DataSource dataSource;
     @Autowired
     BeerRepository beerRepository;
 
-    @DynamicPropertySource
-    static void postgresProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-
-    }
+//    @DynamicPropertySource
+//    static void postgresProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+//        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+//        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+//
+//    }
 
     @Test
     void testListBeers() {
