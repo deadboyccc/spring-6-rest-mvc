7453b76c2f09e88da5fcf69dda03ee7b386b1269
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2025-10-26 18:57:24 +0300

    Add logging to BeerCsvServiceImpl to debug CSV conversion

--- src/main/java/dev/dead/spring6restmvc/services/BeerCsvServiceImpl.java
+++ src/main/java/dev/dead/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -2,6 +2,7 @@ package dev.dead.spring6restmvc.services;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import dev.dead.spring6restmvc.models.BeerCSVRecord;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -9,10 +10,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Slf4j
 @Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
     public List<BeerCSVRecord> convertToCSV(File csvFile) {
+        log.debug("convertCSV() - Service");
         try {
             List<BeerCSVRecord> beerCSVRecords = new CsvToBeanBuilder<BeerCSVRecord>(new FileReader(csvFile))
                     .withType(BeerCSVRecord.class)
