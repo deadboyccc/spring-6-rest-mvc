841712ebb1df2fec1f1774ae9fd58e08d730b77a
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2025-12-20 13:54:53 +0300

    Spring Security 6 prefers the lambda-based configuration.

--- src/main/java/dev/dead/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/dev/dead/spring6restmvc/config/SpringSecConfig.java
@@ -2,18 +2,27 @@ package dev.dead.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
-        http.csrf(httpSecurityCsrfConfigurer ->
-                httpSecurityCsrfConfigurer
-                        .ignoringRequestMatchers("/api/**"));
-        return http.build();
 
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/**")
+                );
+
+        return http.build();
     }
 }
