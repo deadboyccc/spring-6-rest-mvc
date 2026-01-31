9408b1390349d0eaa2815dff3dafe4d989d2db6f
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-01-31 07:30:11 +0300

    Add Springdoc OpenAPI dependencies and update security configuration for API documentation access

--- src/main/java/dev/dead/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/dev/dead/spring6restmvc/config/SpringSecConfig.java
@@ -15,6 +15,8 @@ public class SpringSecConfig {
 
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html")
+                        .permitAll()
                         .anyRequest()
                         .authenticated()
                 )
