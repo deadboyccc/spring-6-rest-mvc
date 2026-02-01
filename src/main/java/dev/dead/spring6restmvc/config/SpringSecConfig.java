c5c16f611771674c9c8ebbb5042a7b5f858f8b40
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-02-01 07:04:53 +0300

    Add actuator and logbook support, update security configuration for public access to actuator and swagger endpoints

--- src/main/java/dev/dead/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/dev/dead/spring6restmvc/config/SpringSecConfig.java
@@ -1,21 +1,35 @@
 package dev.dead.spring6restmvc.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecConfig {
+    // Allowing actuator and swagger endpoints without authentication
+    @Bean
+    @Order(1)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest()
+                        .permitAll());
+
+        return http.build();
+
+    }
 
     @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html")
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
