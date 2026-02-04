c2c209a872137cec56d2049138cecbe41c1106e4
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-02-04 14:14:45 +0300

    Add asynchronous processing for BeerCreatedEvent and update Java version to 25

---
+++ src/main/java/dev/dead/spring6restmvc/config/TaskExecutorConfig.java
@@ -0,0 +1,21 @@
+package dev.dead.spring6restmvc.config;
+
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableAsync
+public class TaskExecutorConfig {
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+}
--- src/main/java/dev/dead/spring6restmvc/listeners/BeerCreatedListener.java
+++ src/main/java/dev/dead/spring6restmvc/listeners/BeerCreatedListener.java
@@ -3,12 +3,14 @@ package dev.dead.spring6restmvc.listeners;
 import dev.dead.spring6restmvc.events.BeerCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class BeerCreatedListener {
     @EventListener
+    @Async
     void listen(BeerCreatedEvent beerCreatedEvent) {
         log.debug(" Beer Created Event Listener - Beer Id: {}",
                 beerCreatedEvent.getBeer()
